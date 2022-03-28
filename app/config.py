100c6152798785bb43a47757bd83714effc2f647
Benedikt-Fischer <bene.aws@gmail.com>
2022-03-28 23:22:40 +0200

    linted config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
-from os import access
+"""
+Create settings object from environment variables for use in database or oauth2.
+Can get the variables from .env file.
+"""
+
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
+    """Settings object, defining variables / data type"""
     database_hostname: str
     database_port: str
     database_name: str
@@ -12,6 +17,7 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int
 
     class Config:
+        """Import variables from .env file, if possible"""
         env_file = ".env"
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 """
 main
 """
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 #from . import models
