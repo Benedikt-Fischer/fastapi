5731b57a7344d01c5d2c6bd209906a37ab742d6c
Benedikt-Fischer <bene.aws@gmail.com>
2022-03-29 01:28:28 +0200

    linted auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,11 @@
+"""
+Auth routes
+"""
+
+from sqlalchemy.orm import Session
 from fastapi import status, HTTPException, Depends, APIRouter
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
+#TODO fix attempted relative import beyond top-level package
 from .. import models, schemas, utils, oauth2
 from ..database import get_db
 
@@ -11,6 +16,8 @@ router = APIRouter(
 @router.post("/login", response_model=schemas.Token)
 #def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """Login user by email / password, verify user exists,
+    verify password, generate and return access_token"""
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
@@ -18,7 +25,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
 
     if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
-    
+
     # create token
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
 
