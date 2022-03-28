6bebec52df82106c064a3d73f0d222ff3b1e5a3d
Benedikt-Fischer <bene.aws@gmail.com>
2022-03-28 23:40:53 +0200

    linted config

--- app/database.py
+++ app/database.py
@@ -1,18 +1,24 @@
+"""
+db driver
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:abcd@localhost/fastapi"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = (f"postgresql://{settings.database_username}:"
+                           f"{settings.database_password}@{settings.database_hostname}:"
+                           f"{settings.database_port}/{settings.database_name}")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    """Try to return session to database"""
+    database = SessionLocal()
     try:
-        yield db
+        yield database
     finally:
-        db.close()
+        database.close()
         
